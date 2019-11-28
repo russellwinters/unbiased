14cd7e686d38331c1138ec89504de462d6bc9f1f
russellwinters <russellwinters@protonmail.com>
2019-11-28 11:51:27 -0800

    Changed how I collect politics articles - through sources

--- server/helpers/api-mongo/politics.js
+++ server/helpers/api-mongo/politics.js
@@ -4,11 +4,65 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 
 let capKey = "cb4bfe6e9f074ed48426d8448833f7e3";
-let many = "pageSize=80";
-const politicsURL = `https://newsapi.org/v2/everything?q=politics&language=en&${many}&apiKey=${capKey}`;
+let many = "pageSize=40";
+let NYTimes = "pageSize=15";
+//strings for everything query in below URLS
+const liberalString = "politico,the-washington-post";
+const theTimes = "the-new-york-times";
+const minimalBias = "abc-news,reuters,usa-today";
+const conservativeString =
+  "fox-news,the-wall-street-journal,national-review,the-hill";
 
+const liberalURL = `https://newsapi.org/v2/everything?language=en&sources=${liberalString}&q=politics&${many}&apiKey=${capKey}`;
+const conservativeURL = `https://newsapi.org/v2/everything?language=en&sources=${conservativeString}&q=politics&${many}&apiKey=${capKey}`;
+const splitURL = `https://newsapi.org/v2/everything?language=en&sources=${minimalBias}&q=politics&${many}&apiKey=${capKey}`;
+const timesURL = `https://newsapi.org/v2/everything?language=en&sources=${theTimes}&q=politics&${NYTimes}&apiKey=${capKey}`;
+
+//split up
 const newArticles = () => {
-  axios.get(politicsURL).then(response => {
+  axios.get(liberalURL).then(response => {
+    tempArr = response.data.articles;
+    tempArr.forEach(obj => {
+      const article = new politicsModel({
+        source: obj.source,
+        author: obj.author,
+        title: obj.title,
+        description: obj.description,
+        url: obj.url,
+        publishedAt: obj.publishedAt
+      });
+      article.save().then(data => console.log(data));
+    });
+  });
+  axios.get(conservativeURL).then(response => {
+    tempArr = response.data.articles;
+    tempArr.forEach(obj => {
+      const article = new politicsModel({
+        source: obj.source,
+        author: obj.author,
+        title: obj.title,
+        description: obj.description,
+        url: obj.url,
+        publishedAt: obj.publishedAt
+      });
+      article.save().then(data => console.log(data));
+    });
+  });
+  axios.get(splitURL).then(response => {
+    tempArr = response.data.articles;
+    tempArr.forEach(obj => {
+      const article = new politicsModel({
+        source: obj.source,
+        author: obj.author,
+        title: obj.title,
+        description: obj.description,
+        url: obj.url,
+        publishedAt: obj.publishedAt
+      });
+      article.save().then(data => console.log(data));
+    });
+  });
+  axios.get(timesURL).then(response => {
     tempArr = response.data.articles;
     tempArr.forEach(obj => {
       const article = new politicsModel({
@@ -23,13 +77,6 @@ const newArticles = () => {
     });
   });
 };
-
-// const sortArticles = () => {
-//   const checkDisplay = politicsModel.find({
-//     author: { $eq: "Alexandra Schwartz" }
-//   });
-//   console.log(checkDisplay);
-// };
 
 module.exports = {
   newArticles

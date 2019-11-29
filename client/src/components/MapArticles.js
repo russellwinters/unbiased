1512bbbc43eef44de6efe8f8e61f20751d2c1bce
russellwinters <russellwinters@protonmail.com>
2019-11-29 09:23:31 -0800

    Blocker on MapArticles - can't get button click to work. Also fixed bug with ESPN/ABC-news mixup

--- client/src/components/MapArticles.js
+++ client/src/components/MapArticles.js
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function MapArticles(props) {
   const articles = props.articles;
+  console.log(props);
   const mapFunction = articles.map(obj => {
     return (
-      <>
+      <div>
         <p>{obj.title}</p>
-        {/* <p>{obj.description}</p>
-        <p>{obj.source.id}</p> */}
-      </>
+        {/* <p>{obj.description}</p> */}
+      </div>
     );
   });
   return <>{mapFunction}</>;
--- server/helpers/api-mongo/sports.js
+++ server/helpers/api-mongo/sports.js
@@ -5,17 +5,35 @@ const axios = require("axios");
 
 let capKey = "cb4bfe6e9f074ed48426d8448833f7e3";
 let many = "pageSize=100";
+let some = "pageSize=50;";
 //Strings for sport sources
-const allSports = "espn,bleacher-report";
+const allSports = "bleacher-report";
 const football = "nfl-news";
 const hockey = "nhl-news";
 //Urls from corresponding news sources
 const sportsURL = `https://newsapi.org/v2/everything?language=en&sources=${allSports}&${many}&apiKey=${capKey}`;
-const footballURL = `https://newsapi.org/v2/everything?language=en&sources=${football}&${many}&apiKey=${capKey}`;
+const espnURL = `https://newsapi.org/v2/top-headlines?language=en&sources=espn&${many}&apiKey=${capKey}`;
+const footballURL = `https://newsapi.org/v2/everything?language=en&sources=${football}&${some}&apiKey=${capKey}`;
 const hockeyURL = `https://newsapi.org/v2/everything?language=en&sources=${hockey}&${many}&apiKey=${capKey}`;
 
 const newArticles = () => {
   axios.get(sportsURL).then(response => {
+    tempArr = response.data.articles;
+    tempArr.forEach(obj => {
+      const article = new sportsModel({
+        source: obj.source,
+        author: obj.author,
+        title: obj.title,
+        description: obj.description,
+        url: obj.url,
+        publishedAt: obj.publishedAt
+      });
+      article
+        .save()
+        .then(data => console.log("Data was sucessfully entered!!"));
+    });
+  });
+  axios.get(espnURL).then(response => {
     tempArr = response.data.articles;
     tempArr.forEach(obj => {
       const article = new sportsModel({

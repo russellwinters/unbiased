e6862f3d324b35ceffa86d04ccb7219314e3d0f7
russellwinters <russellwinters@protonmail.com>
2019-12-16 10:32:59 -0800

    Signup will post to Mongo now. No restrictions on passwords or anything though.

--- client/src/components/profilePages/SignupForm.js
+++ client/src/components/profilePages/SignupForm.js
@@ -1,14 +1,31 @@
 import React from "react";
+import axios from "axios";
 
-export default function LoginForm() {
+export default function SignupForm() {
+  //Basic registration form that's not asking for anything specific from the username or password
+  const register = event => {
+    event.preventDefault();
+    event.persist();
+    let user = {};
+    if (event.target.password.value === event.target.passwordCheck.value) {
+      user.username = event.target.username.value;
+      user.password = event.target.password.value;
+      axios({
+        method: "post",
+        url: "http://localhost:5000/register/api",
+        data: user
+      }).then(response => console.log("success"));
+    } else console.log("error");
+  };
+
   return (
     <section>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={register}>
         <input type="text" name="username" placeholder="Username" />
-        <input type="text" name="password" placeholder="Password" />
+        <input type="password" name="password" placeholder="Password" />
         <input
-          type="text"
+          type="password"
           name="passwordCheck"
           placeholder="Confirm Password"
         />

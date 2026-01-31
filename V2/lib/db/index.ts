82f3e7947f50bd61c18174d930d3eccb1b036ccc
Russell Winters <russellwinters@protonmail.com>
2026-01-31 09:45:26 -0800

    Minor commenting updates

--- V2/lib/db/index.ts
+++ V2/lib/db/index.ts
@@ -2,7 +2,6 @@ export { prisma } from './client'
 export { upsertSources } from './sources';
 export { upsertArticles } from './articles';
 
-// RSS Update History
 export {
   createUpdateHistory,
   completeUpdateHistory,

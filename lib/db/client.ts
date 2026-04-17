52aa508f7831cd2ac843432a27e1a791745f90d8
Russell Winters <russellwinters@protonmail.com>
2026-04-17 11:31:31 -0700

    Updating config to work better with neon

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -1,24 +1,18 @@
 // Database utilities and Prisma client
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
-
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 declare global {
     var prisma: PrismaClient | undefined;
 }
 
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
-
-const adapter = new PrismaPg(pool);
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
 
 export const prisma =
     global.prisma ||
     new PrismaClient({
         adapter,
-        log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+
     });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env("DIRECT_DATABASE_URL"),
   },
 });

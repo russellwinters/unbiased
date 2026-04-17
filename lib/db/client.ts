d9ef36ad410f5c4f52c0fc510829100f7294c1cc
Russell Winters <russellwinters@protonmail.com>
2026-04-17 11:54:27 -0700

    Updates to config and build instructions

--- lib/db/client.ts
+++ lib/db/client.ts
@@ -2,17 +2,5 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from "@prisma/adapter-neon";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-
-export const prisma =
-    global.prisma ||
-    new PrismaClient({
-        adapter,
-
-    });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export const prisma = new PrismaClient({ adapter });
